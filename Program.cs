f3ae41013605821416c1ae237ba2271d1f0e834d
albenchris <albenchris00@gmail.com>
2021-06-10 08:51:12 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CatWorx.BadgeMaker
+{
+    class Program
+    {
+        static void Main(string[] args) // Entry Point
+        // static void Main() // Entry Point
+        {
+            string greeting = "Hello";
+            greeting = greeting + "World";
+            Console.WriteLine("greeting: " + greeting);
+            Console.WriteLine($"greeting {greeting}");
+            Console.WriteLine("greeting: {0}", greeting);
+        }
+    }
+}
