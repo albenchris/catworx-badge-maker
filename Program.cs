73508a7e9a43e2352cfec484917aad56dc7cac36
albenchris <albenchris00@gmail.com>
2021-06-10 13:34:06 -0500

    learning the List data-type and its similarities to JavaScript arrays

--- Program.cs
+++ Program.cs
@@ -9,11 +9,18 @@ namespace CatWorx.BadgeMaker
         // ALSO, THIS...
         // static void Main() // Entry Point
         {
-            string[] favFoods = new string[3] { "pizza", "doughnuts", "icecream" };
-            string firstFood = favFoods[0];
-            string secondFood = favFoods[1];
-            string thirdFood = favFoods[2];
-            Console.WriteLine("I like {0}, {1}, and {2}", firstFood, secondFood, thirdFood);
+            List<string> employees = new List<string>() { "adam", "amy" };
+
+            employees.Add("barbara");
+            employees.Add("billy");
+
+            Console.WriteLine(
+                "My employees include {0}, {1}, {2}, {3}",
+                employees[0],
+                employees[1],
+                employees[2],
+                employees[3]
+            );
         }
     }
 }
