7f14f1f9e409a9808d6c65d11981041bf588ecdd
albenchris <albenchris00@gmail.com>
2021-06-10 09:15:19 -0500

    learning boolean data type

--- Program.cs
+++ Program.cs
@@ -8,20 +8,9 @@ namespace CatWorx.BadgeMaker
         // ALSO, THIS...
         // static void Main() // Entry Point
         {
-            Console.WriteLine(2 * 3);         // Basic Arithmetic: +, -, /, *
-            Console.WriteLine(10 % 3);        // Modulus Op => remainder of 10/3
-            Console.WriteLine(1 + 2 * 3);     // order of operations
-            Console.WriteLine(10 / 3.0);      // int's and doubles
-            Console.WriteLine(10 / 3);        // int's 
-            Console.WriteLine("12" + "3");    // What happens here?
-
-            Console.WriteLine("========================");
-
-            int num = 10;
-            num += 100;
-            Console.WriteLine(num);
-            num++;
-            Console.WriteLine(num);
+            bool isCold = true;
+            Console.WriteLine(isCold ? "drink" : "add ice"); // output: drink
+            Console.WriteLine(!isCold ? "drink" : "add ice"); // output: add ice
         }
     }
 }
