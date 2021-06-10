807d247a13ce3ad98d05a646cad298258c86866c
albenchris <albenchris00@gmail.com>
2021-06-10 13:20:03 -0500

    learning the dictionary data-type

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CatWorx.BadgeMaker
 {
@@ -8,11 +9,23 @@ namespace CatWorx.BadgeMaker
         // ALSO, THIS...
         // static void Main() // Entry Point
         {
-            string stringNum = "2";
-            int intNum = Convert.ToInt16(stringNum);
-            Console.WriteLine("stringNum: {0}, data-type: {1}", stringNum, stringNum.GetType());
+            Dictionary<string, int> myScoreBoard = new Dictionary<string, int>(){
+                { "firstInning", 10 },
+                { "secondInning", 20},
+                { "thirdInning", 30 },
+                { "fourthInning", 40 },
+                { "fifthInning", 50 }
+            };
 
-            Console.WriteLine("intNum: {0}, data-type: {1}", intNum, intNum.GetType());
+            Console.WriteLine("--------------");
+            Console.WriteLine("  Scoreboard  ");
+            Console.WriteLine("--------------");
+            Console.WriteLine("Inning | Score");
+            Console.WriteLine("   1   |   {0}", myScoreBoard["firstInning"]);
+            Console.WriteLine("   2   |   {0}", myScoreBoard["secondInning"]);
+            Console.WriteLine("   3   |   {0}", myScoreBoard["thirdInning"]);
+            Console.WriteLine("   4   |   {0}", myScoreBoard["fourthInning"]);
+            Console.WriteLine("   5   |   {0}", myScoreBoard["fifthInning"]);
         }
     }
 }
