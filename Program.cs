1ce1177c6675f7525a4b242294c722f6f379d46b
albenchris <albenchris00@gmail.com>
2021-07-01 12:55:50 -0500

    program accepts user input

--- Program.cs
+++ Program.cs
@@ -6,15 +6,18 @@ namespace CatWorx.BadgeMaker
     class Program
     {
         static void Main(string[] args) // Entry Point
-        // ALSO, THIS...
-        // static void Main() // Entry Point
+        // ALSO, THIS => static void Main() // Entry Point
+        
         {
             List<string> employees = new List<string>() { "adam", "amy" };
-
             employees.Add("barbara");
             employees.Add("billy");
 
-            Console.WriteLine("My employees are:");
+            Console.WriteLine("Please enter a name: ");
+            // Get a name from the console and assign it to a variable
+            string input = Console.ReadLine();
+            employees.Add(input);
+
             for (int i=0; i<employees.Count; i++)
             {
                 Console.WriteLine(employees[i]);
