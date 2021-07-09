bdeb101f975c67465f3abdefff76db5d70afed0b
albenchris <albenchris00@gmail.com>
2021-07-09 09:37:48 -0500

    moved GetEmployees() method to new PeopleFetcher file

---
+++ PeopleFetcher.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+
+namespace CatWorx.BadgeMaker {
+    class PeopleFetcher
+    {
+        public static List<Employee> GetEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+
+            // Collect user values until the value is an empty string
+            while (true)
+            {
+                // giving some space
+                Console.WriteLine(" ");
+
+                Console.Write("Enter first name (leave empty to exit): ");
+                string firstName = Console.ReadLine();
+                if (firstName == "")
+                {
+                    break;
+                }
+
+                Console.Write("Enter last name: ");
+                string lastName = Console.ReadLine();
+
+                Console.Write("Enter ID: ");
+                int id = Int32.Parse(Console.ReadLine());
+
+                Console.Write("Enter photo URL: ");
+                string photoUrl = Console.ReadLine();
+
+                // Create a new Employee instance
+                Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+                employees.Add(currentEmployee);
+            }
+            return employees;
+        }
+
+        // ===================================================================
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,40 +6,7 @@ namespace CatWorx.BadgeMaker
     class Program
     {
         // =================================================================
-        static List<Employee> GetEmployees()
-        {
-            List<Employee> employees = new List<Employee>();
-
-            // Collect user values until the value is an empty string
-            while (true)
-            {
-                // giving some space
-                Console.WriteLine(" ");
-
-                Console.Write("Enter first name (leave empty to exit): ");
-                string firstName = Console.ReadLine();
-                if (firstName == "")
-                {
-                    break;
-                }
-
-                Console.Write("Enter last name: ");
-                string lastName = Console.ReadLine();
-
-                Console.Write("Enter ID: ");
-                int id = Int32.Parse(Console.ReadLine());
-
-                Console.Write("Enter photo URL: ");
-                string photoUrl = Console.ReadLine();
-
-                // Create a new Employee instance
-                Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
-                employees.Add(currentEmployee);
-            }
-            return employees;
-        }
-
-        // ===================================================================
+        
         
 
 
@@ -47,7 +14,7 @@ namespace CatWorx.BadgeMaker
         static void Main(string[] args)
         // ALSO, THIS => static void Main()
         {
-            List<Employee> employees = GetEmployees();
+            List<Employee> employees = PeopleFetcher.GetEmployees();
             Util.PrintEmployees(employees);
             Util.MakeCSV(employees);
             Util.MakeBadges(employees);
